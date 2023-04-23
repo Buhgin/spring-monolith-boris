5d68d07848832fc789e90d8d80ed2b1b6c9c3075
Boris <3eva@bk.ru>
2023-04-23 13:35:38 +0600

    Initial commit

---
+++ src/main/java/com/boris/SpringMonolithBorisApplication.java
@@ -0,0 +1,13 @@
+package com.boris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonolithBorisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMonolithBorisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boris/SpringMonolithBorisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonolithBorisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
